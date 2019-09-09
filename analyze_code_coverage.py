e76c395c37597959bc49b44e1510dc6cd8e28c96
Dirtybird123 <35776445+Dirtybird123@users.noreply.github.com>
2019-09-09 20:11:17 +0100

    Pipeline Change

--- analyze_code_coverage.py
+++ analyze_code_coverage.py
@@ -5,8 +5,21 @@ if __name__ == '__main__':
     # get root element
     root = ET.parse("coverage.xml").getroot()
 
+    failed = False
+
     # iterate news items
     for package in root.findall('./packages/package'):
         for cls in package.findall('./classes/class'):
             if float(cls.attrib['line-rate']) < 0.5:
-                raise Exception("File '%s' failed the coverage check. Code coverage must be above 50%%." % cls.attrib['filename'])
+                print(False)
+                failed = True
+                break
+
+        if failed:
+            break
+
+    if not failed:
+        print(True)
+
+
+            #raise Exception("File '%s' failed the coverage check. Code coverage must be above 50%%." % cls.attrib['filename'])
