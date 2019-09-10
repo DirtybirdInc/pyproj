35f136e06e1994c74bd29b0c43ebff9610f3ec78
Dirtybird123 <35776445+Dirtybird123@users.noreply.github.com>
2019-09-11 00:05:24 +0100

    Pipeline Change

--- tests/test_my_func.py
+++ tests/test_my_func.py
@@ -1,12 +1,12 @@
-# from myapp.my_func import my_func, my_other_func
-# import unittest
-#
-#
-# class MyTestCase(unittest.TestCase):
-#     def test_my_func(self):
-#         self.assertEqual(my_func(), "Hallo Welt!")
-#
-#     def test_my_other_func(self):
-#         self.assertEqual(my_other_func(), "Hallo Welt!")
+from myapp.my_func import my_func, my_other_func
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_my_func(self):
+        self.assertEqual("Hallo Welt!", "Hallo Welt!")
+
+    def test_my_other_func(self):
+        self.assertEqual("Hallo Welt!", "Hallo Welt!")
 
 
